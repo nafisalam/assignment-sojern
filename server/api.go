00b5877d8bec8c359b67deb1bbdd55572b9c8ca1
nafisalam <naf_alm@yahoo.com>
2022-06-18 10:21:48 -0700

    updated comments

--- server/api.go
+++ server/api.go
@@ -14,6 +14,7 @@ type webconfig struct {
 	Cong *config.Config
 }
 
+// NewRouter to create a new router with all the apis end
 func NewRouter(cfg config.Config) *mux.Router {
 	r := mux.NewRouter()
 	wc := webconfig{&cfg}
@@ -22,8 +23,9 @@ func NewRouter(cfg config.Config) *mux.Router {
 	return r
 }
 
+// ping is the handler to server the ping requests
 func (c webconfig) ping(w http.ResponseWriter, r *http.Request) {
-	c.LogRequet(r)
+	c.LogRequest(r)
 	ok := util.CheckFile(c.Cong.StatusFilePath)
 	if ok {
 		w.WriteHeader(http.StatusOK)
@@ -36,8 +38,9 @@ func (c webconfig) ping(w http.ResponseWriter, r *http.Request) {
 	w.Write(b)
 }
 
+// img is the handler to server /img requests
 func (c webconfig) img(w http.ResponseWriter, r *http.Request) {
-	c.LogRequet(r)
+	c.LogRequest(r)
 	b, err := util.GetImageFromCache()
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -49,7 +52,8 @@ func (c webconfig) img(w http.ResponseWriter, r *http.Request) {
 	log.Println("End: Request for a GIF image ")
 }
 
-func (c webconfig) LogRequet(r *http.Request) {
+// LogRequet is to dump the requests to a log file.
+func (c webconfig) LogRequest(r *http.Request) {
 	request, err := httputil.DumpRequest(r, true)
 	if err != nil {
 		util.LogData("error to read the request ", c.Cong.LogsPath)
